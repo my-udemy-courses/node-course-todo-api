3c442651cc8975777f7381587da70839ef1b9a07
Arthuro <artgrohe@googlemail.com>
2019-01-15 19:28:16 +0100

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,24 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-// empty database before each test
+// prepare database, so for every new test session it has the same data!
+
+const dummyTodos = [
+    { text: "First test todo" },
+    { text: "Second test todo" },
+    { text: "Third test todo" }
+]
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
-})
+    // First clear entire Todos collection
+    Todo.remove({})
+    // Then insert dummy collection
+    .then(() => {
+        return Todo.insertMany(dummyTodos);
+    })
+    // Then we are done
+    .then(() => done());
+});
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
@@ -26,7 +40,7 @@ describe('POST /todos', () => {
                 }
 
                 // Verify that the doc was inserted into db
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -44,9 +58,21 @@ describe('POST /todos', () => {
 
                 // Verify that no doc was inserted to db
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(dummyTodos.length);
                     done()
                 }).catch((err) => done(err));
             })
+    });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(dummyTodos.length);
+            })
+            .end(done);
     });
 });
